06aabaa44e090e5917f6d61586f92833196611ec
akhildevelops <akhilgodvsdemon@gmail.com>
2024-03-17 13:20:53 +0530

    coverage

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Wisdom
 Answers all your company related queries.
+
+# Testing:
+Run `cargo test`
+
+# Code Coverage:
+- `rustup component adddd llvm-tools-preview`
+- `RUSTFLAGS="-Cinstrument-coverage" LLVM_PROFILE_FILE="./target/wisdom-%p-%m.profraw" cargo test`
+- `grcov . -s . --binary-path ./target/debug/ -t html --branch -o ./target/debug/coverage/`
--- src/documents/pdf.rs
+++ src/documents/pdf.rs
@@ -15,3 +15,17 @@ impl Document for PDF {
         })
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::fs;
+    #[test]
+    fn pdf_load() {
+        let dummy_pdf = "test/artifacts/dummy.pdf";
+        let file_reader = fs::OpenOptions::new().read(true).open(dummy_pdf).unwrap();
+        let pdf_doc = PDF::load(file_reader).unwrap();
+        let text = pdf_doc.document.extract_text(&[1]).unwrap();
+        assert_eq!("Hello World\n", text);
+    }
+}
